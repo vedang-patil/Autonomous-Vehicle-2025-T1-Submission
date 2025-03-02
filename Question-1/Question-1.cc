3eb667a4b731904146b5f427dc6d2fa5e99e03fa
Vedang Patil <vedang1006@gmail.com>
2025-03-02 16:55:57 +1100

    Question 1 Solved

--- Question-1/Question-1.cc
+++ Question-1/Question-1.cc
@@ -2,85 +2,86 @@
 /// the comments given inside the code.
 /// Your code should run and compile without any warnings or errors.
 
-
 #include <iostream>
+#include <cmath>
 #include <map>
 #include <vector>
 #include <initializer_list>
+#include <memory>
+#include <numeric>
 
-const int values[] = { 1,2,3,4,5 };
-const int val_size = sizeof values / sizeof(int);
+const int values[] = {1, 2, 3, 4, 5};
+const int val_size = (sizeof(values) / sizeof(int));
 
-
-
-class Foo {
+class Foo
+{
     std::vector<int> mElements;
+
 public:
     Foo(std::initializer_list<int> list)
-    : mElements(list) {}
+        : mElements(list) {}
 };
 
-struct {
+struct Person
+{
     int age;
     float weight;
-} person;
+};
 
-
-void show_output(const int** pp)
+void show_output(const int **pp)
 {
-    printf("%p : %p : %d", pp, *pp, **pp);
+    printf("%p : %p : %d", (void*)pp, (void*)*pp, **pp);
 }
 
+void show_output(const int *pp)
+{
+    printf("%p : %d", (void*)pp, *pp);
+}
 
-
-int main() {
-
+int main()
+{
     int x = -10;
     int y = 15;
-    cout << " " << (x,y) << std::endl;
+    std::cout << " (" << x << ", " << y << ")" << std::endl;
 
     // print integer ratios of y:x till x < y
     // invalid ratios should print 0
+
     while (x < y)
     {
-        cout << "ratio: " << (y/x) << endl;
+        if (x == 0) std::cout << "ratio: 0" << std::endl;
+        else std::cout << "ratio: " << (x / std::gcd(x, y)) << ":" << (y / std::gcd(x, y)) << std::endl;
         x++;
         y--;
     }
 
-
     int i = 1, j = 1; // don't change values
-    if ((i = !3) & (j = 1))
+    if ((i != 3) && (j == 1))
     {
-        cout << "j is 1 and i is not 3\n";
+        std::cout << "j is 1 and i is not 3\n";
     }
-    else {
-        cout << "either j is NOT 1, or i is set to 3\n";
-
+    else
+    {
+        std::cout << "either j is NOT 1, or i is set to 3\n";
     }
 
-
-    typedef map<int, double> valmap;
+    typedef std::map<int, double> valmap;
     valmap m;
 
-    for (int i = 0; i < valSize; i++)
-        m.insert(make_pair(values[i], pow(values[i], .5)));
-
-    m.insert(1, 2);
-
+    for (int i = 0; i < val_size; i++) m.emplace(values[i], std::pow(values[i], 0.5));
+    m[1] = 2;
 
     int n = 1;
-    int* p = &n;
+    int *p = &n;
     show_output(p);
 
     // Initialise a person on the heap with the use of smart pointers (unique_ptr) instead.
-    struct person* ptr;
-    ptr = (struct person*)malloc(sizeof(struct person));
+    std::unique_ptr<Person> ptr = std::make_unique<Person>();
     ptr->age = 10;
     ptr->weight = 55.5;
 
     // Initialise with 5 integers
-    Foo foo;
+    Foo foo({1, 2, 3, 4, 5});
 
     return 0;
 }
